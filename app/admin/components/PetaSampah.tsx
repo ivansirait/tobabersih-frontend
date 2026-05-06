36c832b7f54930dd376b1f96636e106a71d9075a
David GFS <sinuratdavid949@gmail.com>
2026-05-06 21:27:56 +0700

    fix error socket peta sampah

--- app/admin/components/PetaSampah.tsx
+++ app/admin/components/PetaSampah.tsx
@@ -1,7 +1,7 @@
 "use client";
 import { useEffect, useState, useRef } from 'react';
 import axios from 'axios';
-import { io, Socket } from 'socket.io-client';
+
 import { 
   Map as MapIcon, 
   Truck, 
